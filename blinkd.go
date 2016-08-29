1d5084d97daa9bde408d25b224e6130422e7cac5
Ted Smith <tedks@cs.umass.edu>
2016-08-28 19:58:06 -0700

    basic setstate test

--- blinkd.go
+++ blinkd.go
@@ -1,6 +1,10 @@
-package blinkd
+package main
 
-import "github.com/hink/go-blink1"
+import (
+	"fmt"
+	"time"
+	b1 "github.com/hink/go-blink1"
+)
 
 const (
     USBVendorID  = 10168
@@ -8,6 +12,12 @@ const (
 )
 
 func main() {
-	
+	blink, err := b1.OpenNextDevice()
+	if err != nil {
+		fmt.Println("Couldn't open Blink")
+		return
+	}
+	blink.SetState(b1.State{Red: 255, Duration: time.Second * 30})
+	fmt.Println("Called setState, exiting")
 }
 
